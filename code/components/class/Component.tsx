589d7d95875b1c4bee9be4c0944f6a6a29abb98c
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-04-22 22:41:02 +0530

    example files adding

---
+++ code/components/class/Component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Component extends React.Component {
+  render() {
+    return <p>React Component</p>;
+  }
+}
+
+export { Component };
+export default Component;
---
+++ code/components/class/PureComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class PureComponent extends React.Component {
+  render() {
+    return <p>React Pure Component</p>;
+  }
+}
+
+export { PureComponent };
+export default PureComponent;
