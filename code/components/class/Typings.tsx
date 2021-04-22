141d2577c39c72d3cf42c6e0bf8355f897279d0e
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-04-22 23:01:01 +0530

    typings example added

---
+++ code/components/class/Typings.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * @description
+ * - using javascript
+ * - using propTypes package for typings
+ */
+export class Car extends React.Component {
+  render() {
+    return <div>{JSON.stringify(this.props)}</div>;
+  }
+}
+
+Car.propTypes = {
+  name: PropTypes.oneOf(["bmw", "audi", "jaquar"]),
+  model: PropTypes.string,
+  number: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  isFourWheelDriving: PropTypes.bool
+};
+
+/**
+ * @description
+ * using typescript
+ */
+
+type TCarName = "bmw" | "audi" | "jaquar";
+
+interface ICarProps {
+  name: TCarName;
+  model: string;
+  number: number | string;
+  isFourWheelDriving?: boolean;
+}
+
+export class TypescriptCar extends React.Component<ICarProps> {
+  render() {
+    return <div>{JSON.stringify(this.props)}</div>;
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import {
+  Component,
+  PureComponent,
+  Car,
+  TypescriptCar
+} from "../code/components/class";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>React JS</h1>
+      <hr />
+      <h3>Components</h3>
+      <Component />
+      <PureComponent />
+      <hr />
+      <h3>Components: PropTypes</h3>
+      <Car
+        name="bmw"
+        model="x1"
+        number="TN 00 1829"
+        isFourWheelDriving={true}
+      />
+      <TypescriptCar
+        name="bmw"
+        number="TN 00 1829"
+        model="x1"
+        isFourWheelDriving={true}
+      />
     </div>
   );
 }
