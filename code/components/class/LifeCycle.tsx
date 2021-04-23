cfbf85824bf4f18ae7b18074f67954a2cb91e201
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-04-23 12:14:53 +0530

    life cycle explanation added

--- code/components/class/LifeCycle.tsx
+++ code/components/class/LifeCycle.tsx
@@ -38,26 +38,67 @@ export class ImageLoader extends React.Component<IImageLoaderProps> {
 
   /**
    * @description
-   * - will be triggered when the state has updated
+   * - before render() will be called
+   * - compares the state & props and returns updated object
+   * - returns `null` if nothing required to be updated
+   *
+   * @param props
+   * @param state
    */
-  componentDidUpdate() {
-    console.log(this.state);
+  static getDerivedStateFromProps(props: any, state: any) {
+    console.log({ props, state });
+    return null;
   }
 
   /**
    * @description
-   * - will be triggered when component going to removed from the page
+   * - will be called after render()
+   * - before the DOM going to be changed
+   *
+   * @param prevProps
+   * @param prevState
    */
-  componentWillUnmount() {}
+  getSnapshotBeforeUpdate(prevProps: any, prevState: any) {
+    console.log({ prevProps, prevState });
+    return null;
+  }
 
   /**
    * @description
-   * - will be triggered only one time
+   * - will be called before calling the render() and after the getDerivedStateFromProps()
+   * - if returns `true`  render() will be called else will be stopped
+   *
+   * @param nextProps
+   * @param nextState
+   */
+  shouldComponentUpdate(nextProps: any, nextState: any) {
+    console.log({ nextProps, nextState });
+    return true;
+  }
+
+  /**
+   * @description
+   * - will be called when the state has updated
+   * - beware: when adding state changes here it will endup with infinite loop - should have proper condition
+   */
+  componentDidUpdate(prevProps: any, prevState: any, snapshot: any) {
+    console.log({ prevProps, prevState, snapshot });
+  }
+
+  /**
+   * @description
+   * - will be called only one time
    * - when the components gets loaded/mounted in the page
    */
   componentDidMount() {
     this.updateState("Component Mounted!");
   }
+
+  /**
+   * @description
+   * - will be called when component going to removed from the page
+   */
+  componentWillUnmount() {}
 
   render() {
     return (
