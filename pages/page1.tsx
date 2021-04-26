dde9d58fd51c96cf7917a7ca550373f6cedc1333
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-04-26 14:01:21 +0530

    basic routing added in app

---
+++ pages/page1.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Component,
+  PureComponent,
+  Car,
+  TypescriptCar,
+  Counter,
+  ImageLoader,
+  LogicalShape,
+  ConditionalShape,
+  ElementVariables,
+  FlowerGallery,
+  TodoList
+} from "../code/components/class";
+
+export default class Page1 extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React JS</h1>
+        <hr />
+        <h3>Components</h3>
+        <Component />
+        <PureComponent />
+        <hr />
+        <h3>Class Components: PropTypes/Typings</h3>
+        <Car
+          name="bmw"
+          model="x1"
+          number="TN 00 1829"
+          isFourWheelDriving={true}
+        />
+        <TypescriptCar
+          name="bmw"
+          number="TN 00 1829"
+          model="x1"
+          isFourWheelDriving={true}
+        />
+        <hr />
+        <h3>Class Components: State</h3>
+        <Counter initialCount={0} />
+        <hr />
+        <h3>Class Components: Lifecycle</h3>
+        <ImageLoader url="https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg" />
+        <hr />
+        <h3>Conditional Rendering</h3>
+        <p>Logical Operator ( && )</p>
+        <LogicalShape shapeType="square" />
+        <LogicalShape shapeType="circle" />
+        <p>Conditinal Operation ( ? : )</p>
+        <ConditionalShape shapeType="square" />
+        <ConditionalShape shapeType="circle" />
+        <p>Using Variables for Elements/Components</p>
+        <ElementVariables shapeType="square" />
+        <ElementVariables shapeType="circle" />
+        <p>Context & Provider</p>
+        <FlowerGallery />
+        <p>Lists & Keys</p>
+        <TodoList />
+      </div>
+    );
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,15 @@
-import {
-  Component,
-  PureComponent,
-  Car,
-  TypescriptCar,
-  Counter,
-  ImageLoader,
-  LogicalShape,
-  ConditionalShape,
-  ElementVariables,
-  FlowerGallery,
-  TodoList
-} from "../code/components/class";
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Page1 from "../pages/page1";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>React JS</h1>
-      <hr />
-      <h3>Components</h3>
-      <Component />
-      <PureComponent />
-      <hr />
-      <h3>Class Components: PropTypes/Typings</h3>
-      <Car
-        name="bmw"
-        model="x1"
-        number="TN 00 1829"
-        isFourWheelDriving={true}
-      />
-      <TypescriptCar
-        name="bmw"
-        number="TN 00 1829"
-        model="x1"
-        isFourWheelDriving={true}
-      />
-      <hr />
-      <h3>Class Components: State</h3>
-      <Counter initialCount={0} />
-      <hr />
-      <h3>Class Components: Lifecycle</h3>
-      <ImageLoader url="https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg" />
-      <hr />
-      <h3>Conditional Rendering</h3>
-      <p>Logical Operator ( && )</p>
-      <LogicalShape shapeType="square" />
-      <LogicalShape shapeType="circle" />
-      <p>Conditinal Operation ( ? : )</p>
-      <ConditionalShape shapeType="square" />
-      <ConditionalShape shapeType="circle" />
-      <p>Using Variables for Elements/Components</p>
-      <ElementVariables shapeType="square" />
-      <ElementVariables shapeType="circle" />
-      <p>Context & Provider</p>
-      <FlowerGallery />
-      <p>Lists & Keys</p>
-      <TodoList />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Page1 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
