31790ab7abe44f4022d92d49fca6b0f7817f4adc
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2022-05-05 14:20:27 +0530

    xss issue fix added for multiple encoded urls

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,34 @@ export default function App() {
   const brokenURL =
     "?sort=%7b%22sort%22%3a[%7b%22field%22%3a%22a74qo%3c%5c%2fscript%3e%3cscript%3ealert(1)%3c%5c%2fscript%3ebpbkb%22%2c%22direction%22%3a%22ASC%22%7d%2c%7b%22field%22%3a%22sort_number_price%22%2c%22direction%22%3a%22ASC%22%7d]%7d";
 
-  const decodeURL = decodeURIComponent(brokenURL);
+  const multiEncodeURL =
+    // "sort%3D%257b%2522sort%2522%253a%5B%257b%2522field%2522%253a%2522a74qo%253c%255c%252fscript%253e%253cscript%253ealert(1)%253c%255c%252fscript%253ebpbkb%2522%252c%2522direction%2522%253a%2522ASC%2522%257d%252c%257b%2522field%2522%253a%2522sort_number_price%2522%252c%2522direction%2522%253a%2522ASC%2522%257d%5D%257d";
+    "sort%2525252525253D%252525252525257b%2525252525252522sort%2525252525252522%252525252525253a%2525252525255B%252525252525257b%2525252525252522field%2525252525252522%252525252525253a%2525252525252522a74qo%252525252525253c%252525252525255c%252525252525252fscript%252525252525253e%252525252525253cscript%252525252525253ealert(1)%252525252525253c%252525252525255c%252525252525252fscript%252525252525253ebpbkb%2525252525252522%252525252525252c%2525252525252522direction%2525252525252522%252525252525253a%2525252525252522ASC%2525252525252522%252525252525257d%252525252525252c%252525252525257b%2525252525252522field%2525252525252522%252525252525253a%2525252525252522sort_number_price%2525252525252522%252525252525252c%2525252525252522direction%2525252525252522%252525252525253a%2525252525252522ASC%2525252525252522%252525252525257d%2525252525255D%252525252525257d";
+
+  const decodeURLToBase = (url) => {
+    let tempDecodeURL = url;
+    while (tempDecodeURL !== decodeURIComponent(tempDecodeURL)) {
+      tempDecodeURL = decodeURIComponent(tempDecodeURL);
+    }
+    return tempDecodeURL;
+  };
+
+  const decodeURL = decodeURLToBase(multiEncodeURL);
+  // console.log({ decodeURL });
+
+  console.log("complete decode url: ", decodeURLToBase(multiEncodeURL));
+
   const xssCleaned = xss(decodeURL, {
     whiteList: {},
-    onIgnoreTag: () => "",
     stripIgnoreTag: true, // filter out all HTML not in the whilelist
     stripIgnoreTagBody: ["script"]
   });
+
   console.log({ decodeURL });
   console.log("xss", xssCleaned);
 
-  const encodeURL = encodeURIComponent(xssCleaned);
-  console.log({ encodeURL });
+  // const encodeURL = encodeURIComponent(xssCleaned);
+  // console.log({ encodeURL });
   // console.log({ decodeURL });
   // const sanitizeURL = sanitizeHtml(decodeURL);
 
